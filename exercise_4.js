1157f74b892346375ec8857b8fca66885991b048
Gugea David <davidgugea@yahoo.com>
2020-06-22 19:37:15 +0300

    Exercise 4 done

---
+++ exercise_4.js
@@ -0,0 +1,10 @@
+/*
+4. Write a JavaScript function that returns a passed string with letters in alphabetical order. Go to the editor
+Example string : 'webmaster'
+Expected Output : 'abeemrstw'
+Assume punctuation and numbers symbols are not included in the passed string.
+*/
+
+let sortString = string => string.split("").sort().join("");
+
+console.log(sortString("webmaster"));
