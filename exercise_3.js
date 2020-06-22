b5c44fb2b671d5a4fda5c51d8c751333d8f61e04
Gugea David <davidgugea@yahoo.com>
2020-06-22 19:18:01 +0300

    Not enough knowdledge about algorithms and recursion

---
+++ exercise_3.js
@@ -0,0 +1,9 @@
+/*
+3. Write a JavaScript function that generates all combinations of a string. Go to the editor
+Example string : 'dog'
+Expected Output : d,do,dog,o,og,g
+*/
+
+let makeCombinations = string => {
+	
+}
