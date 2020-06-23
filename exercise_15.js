49eaa11371c26d7ecf6b57d3e6f8a37f34d99c8d
Gugea David <davidgugea@yahoo.com>
2020-06-23 19:32:49 +0300

    Exercise 15 done

---
+++ exercise_15.js
@@ -0,0 +1,7 @@
+/*
+15. Write a JavaScript function to compute the value of bn where n is the exponent and b is the bases. Accept b and n from the user and display the result.
+*/
+
+let exponent = (b, n) => Math.pow(b, n);
+
+console.log(exponent(2, 5));
