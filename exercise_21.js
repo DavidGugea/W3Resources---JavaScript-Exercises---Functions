944abb7032dc7cbf76539e12b5a1c00297585efc
Gugea David <davidgugea@yahoo.com>
2020-06-23 20:00:12 +0300

    Not enough knowdlege about algorithms ( backtracking etc. )

---
+++ exercise_21.js
@@ -0,0 +1,11 @@
+/*
+21. Write a JavaScript function to get all possible subset with a fixed length (for example 2) combinations in an array. 
+*/
+
+let subsetCombinations = (arr, subset_length) => {
+	let combinations = new Array();
+
+	arr.map(chr => {
+		
+	});
+};
